083e613778af12b1d57637787b9a99907bbe292b
dmayerdesign <d.a.mayer92@gmail.com>
2019-08-22 21:47:25 -0400

    Complete first pass at Redux Devtools support

--- projects/vex/src/lib/vex.ts
+++ projects/vex/src/lib/vex.ts
@@ -1,7 +1,8 @@
-import { from, of, Observable, Subject } from 'rxjs'
-import { catchError, concatMap, filter, first, map, mergeScan, scan, share, shareReplay, startWith, tap, withLatestFrom } from 'rxjs/operators'
-import { DevtoolsOptions, NgZoneLike } from './akita-devtools'
-import { configureDevtoolsForInstance } from './devtools'
+import { empty, from, merge, of, Observable, Subject } from 'rxjs'
+import { catchError, concatMap, filter, first, map, mergeScan, scan, share, shareReplay, startWith, switchMap, tap, withLatestFrom } from 'rxjs/operators'
+import { DevtoolsOptions } from './akita-devtools'
+
+let globalSyncOnlyVex: Vex<any>
 
 export interface VexOptions {
   allowConcurrency?: boolean,
@@ -49,8 +50,7 @@ export class Vex<StateType> {
 
   constructor(
     private _initialState: StateType,
-    { allowConcurrency, devtoolsOptions }: VexOptions = { allowConcurrency: true },
-    ngZone?: NgZoneLike,
+    { allowConcurrency }: VexOptions = { allowConcurrency: true },
     featureKey?: string,
   ) {
     const initialResult: ActionResult<StateType> = {
@@ -60,7 +60,7 @@ export class Vex<StateType> {
 
     if (!allowConcurrency) {
       this._resolution$ = this._actionß.pipe(
-        tap((action) => this._actionAuditß.next({ ...action, featureKey })),
+        tap((action) => this._actionAuditß.next(Object.assign({ ...action, featureKey }))),
         withLatestFrom(this.state$ || of(this._initialState)),
         concatMap(([action, state]) => this._resolve(state, action)),
         scan(
@@ -73,7 +73,7 @@ export class Vex<StateType> {
     }
     else {
       this._resolution$ = this._actionß.pipe(
-        tap((action) => this._actionAuditß.next({ ...action, featureKey })),
+        tap((action) => this._actionAuditß.next(Object.assign({ ...action, featureKey }))),
         mergeScan<Action<StateType, any>, ActionResult<StateType>>(
           ({ state } = initialResult, action) => this._resolve(state, action),
           initialResult,
@@ -93,9 +93,7 @@ export class Vex<StateType> {
     configureDevtoolsForInstance(
       featureKey,
       this._dispatchAudit$,
-      this._resolution$,
-      { allowConcurrency, devtoolsOptions },
-      ngZone
+      this._resolution$
     )
   }
 
@@ -194,19 +192,186 @@ export class Vex<StateType> {
   }
 }
 
+globalSyncOnlyVex = new Vex({}, { allowConcurrency: false })
+
 export function createVex(
   initialState: any,
   options: VexOptions,
-  ngZone?: NgZoneLike
 ): Vex<any> {
-  return new Vex(initialState, options, ngZone)
+  setUpDevtools(options.devtoolsOptions)
+  const vex = new Vex(initialState, options)
+  return vex
 }
 
 export function createVexForFeature(
   featureKey: string,
   initialState: any,
   options: VexOptions,
-  ngZone?: NgZoneLike
 ): Vex<any> {
-  return new Vex(initialState, options, ngZone, featureKey)
+  return new Vex(initialState, options, featureKey)
 }
+
+
+
+
+
+
+
+/** Singleton. */
+let devTools: any
+const $$dispatch$$ = new Subject<Observable<AuditableAction<any, any>>>()
+const $$resolution$$ = new Subject<Observable<ActionResult<any>>>()
+const $$dispatch$ = $$dispatch$$.pipe(
+  mergeScan(
+    (mergedDispatch$, dispatch$) => of([ ...mergedDispatch$, dispatch$ ]),
+    [ empty() ] as Observable<AuditableAction<any, any>>[]
+  ),
+  switchMap((dispatch$List) => merge(...dispatch$List)),
+)
+const $$resolution$ = $$resolution$$.pipe(
+  mergeScan(
+    (mergedresolution$, resolution$) => of([ ...mergedresolution$, resolution$ ]),
+    [ empty() ] as Observable<ActionResult<any>>[]
+  ),
+  switchMap((resolution$List) => merge(...resolution$List)),
+)
+
+export function setUpDevtools(
+  devtoolsOptions?: DevtoolsOptions,
+): void {
+  if (!window) return
+  if (!(window as any).__REDUX_DEVTOOLS_EXTENSION__) {
+    return
+  }
+  if (devTools) {
+    throw new Error('Attempted to call `setUpDevtools` more than once.')
+  }
+
+  const defaultOptions: Partial<DevtoolsOptions> & { name: string } = { name: 'Vex', shallow: true }
+  const mergedOptions = Object.assign({}, defaultOptions, devtoolsOptions)
+  devTools = (window as any).__REDUX_DEVTOOLS_EXTENSION__.connect(mergedOptions)
+
+  const devtoolsDispatch$ = $$dispatch$.pipe(
+    switchMap(({ type }) => {
+      return globalSyncOnlyVex.once({
+        type: '[DISPATCHED] ' + type,
+        resolve: async () => undefined,
+        mapToState: (globalState) => globalState
+      })
+    }),
+    map(({ actionType, featureKey }) => [ actionType, featureKey ])
+  )
+
+  const devtoolsResult$ = $$resolution$.pipe(
+    switchMap(({ actionType, featureKey, state, error }) => {
+      return globalSyncOnlyVex.once({
+        type: (error ? '[RESOLVED (with error)] ' : '[RESOLVED] ') + actionType,
+        resolve: async () => undefined,
+        mapToState: (globalState) => mapFeatureStateToGlobal(featureKey, globalState, state),
+      })
+    }),
+    map(({ actionType, featureKey }) => [ actionType, featureKey ])
+  )
+
+  merge(devtoolsDispatch$, devtoolsResult$)
+    .pipe(withLatestFrom(globalSyncOnlyVex.state$))
+    .subscribe(([ [ type, featureKey ], state ]) => {
+      const message = { type: `[${featureKey || 'APP_ROOT'}] ${type}` }
+      devTools.send(message, state)
+      if (devtoolsOptions && devtoolsOptions.logTrace) {
+        console.group(JSON.stringify(message))
+        // tslint:disable-next-line
+        console.trace();
+        console.groupEnd()
+      }
+    })
+
+  // if (subs.length) {
+  //   subs.forEach(s => {
+  //     if (s.unsubscribe) {
+  //       s.unsubscribe()
+  //     } else if (typeof s === 'function') {
+  //       s()
+  //     }
+  //   })
+  // }
+
+  // subs.push(
+  //   $$deleteStore.subscribe(storeName => {
+  //     delete appState[storeName];
+  //     devTools.send({ type: `[${storeName}] - Delete Store` }, appState);
+  //   })
+  // );
+
+  devTools.subscribe((message) => {
+    // if (message.type === 'ACTION') {
+    //   const [storeName] = message.payload.split('.')
+
+    //   if(__stores__[storeName]) {
+    //     (ngZoneOrOptions as NgZoneLike).run(() => {
+    //       const funcCall = message.payload.replace(storeName, `this['${storeName}']`);
+    //       try {
+    //         new Function(`${funcCall}`).call(__stores__);
+    //       } catch(e) {
+    //         console.warn('Unknown Method ☹️');
+    //       }
+    //     });
+    //   }
+    // }
+
+    if (message.type === 'DISPATCH') {
+      const payloadType = message.payload.type
+
+      if (payloadType === 'COMMIT') {
+        globalSyncOnlyVex.state$.pipe(first()).subscribe((appState) => {
+          devTools.init(appState)
+        })
+        return
+      }
+
+      if (message.state) {
+        globalSyncOnlyVex.dispatch({ type: '[devtools] ' + message.type, resolve: () => message.state })
+      }
+    }
+  })
+}
+
+function mapFeatureStateToGlobal(_featureKey: string | undefined, globalState: any, featureState: any): any {
+  const featureKey = _featureKey || ''
+  const lookups = featureKey.split('.')
+
+  if (lookups.length === 0 && lookups[0] === '') {
+    // Is root.
+    return featureState
+  }
+
+  const newGlobalState = { ...globalState }
+  let parentObject = newGlobalState
+  const allButLastLookup = lookups.slice(0, lookups.length - 1)
+  const lastLookup = lookups[lookups.length - 1]
+  allButLastLookup.forEach((lookup) => {
+    if (!parentObject[lookup]) {
+      parentObject[lookup] = {}
+    }
+    parentObject = parentObject[lookup]
+  })
+  parentObject[lastLookup] = featureState
+
+  return newGlobalState
+}
+
+/** @internal */
+export function configureDevtoolsForInstance<StateType, ResultType>(
+  featureKey: string | undefined,
+  dispatch$: Observable<Action<StateType, ResultType>>,
+  resolution$: Observable<ActionResult<StateType>>,
+): void {
+  if (globalSyncOnlyVex) {
+    globalSyncOnlyVex.state$.pipe(first()).subscribe((appState) => {
+      devTools.send({ type: `[${featureKey || 'APP_ROOT'}] @@INIT` }, appState)
+      $$dispatch$$.next(dispatch$ as Observable<AuditableAction<StateType, ResultType>>)
+      $$resolution$$.next(resolution$ as Observable<ActionResult<StateType>>)
+    })
+  }
+}
+
